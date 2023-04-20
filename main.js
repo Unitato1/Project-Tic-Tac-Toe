66947dc77ea72d3c03b24661594d4d1091aec99e
Unitato1 <robertsvihla@gmail.com.github.com>
2023-04-20 21:40:57 +0200

    Can't play occupied field

--- main.js
+++ main.js
@@ -11,19 +11,47 @@ const gameBoard = (() => {
       field = document.createElement("div");
       field.dataset.pos = i;
       field.addEventListener("mousedown", function () {
-        console.log(game.curr);
-        this.textContent = game.curr;
-        game.curr = game.curr === "X" ? "O" : "X";
+        if (this.textContent === "") {
+          this.textContent = game.curr;
+          game.curr = game.curr === "X" ? "O" : "X";
+          console.log(game.checkWinner());
+        }
       });
       dom_Board.appendChild(field);
       Board.push(field);
     }
+    return Board;
   };
-  return { createBoard };
+  return { Board, createBoard };
 })();
-
+// ibistek a duha
+// slonik a blcha
+// hviezdicka a slnieckom
+// palmicka a vlna morska
+// prasatko a tuipan
+// chopotniska a maco
+// velke bu a male bu
 const game = (() => {
   const curr = "X";
+  let start_symbol;
   gameBoard.createBoard();
-  return { curr };
+  const board = gameBoard.Board;
+  const checkLine = (start, end, step) => {
+    console.log(board[start]);
+    start_symbol = gameBoard.Board[start].textContent;
+    for (let i = start + step; start < end; i = +step) {
+      if (start_symbol !== gameBoard.Board[i].textContent) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const checkWinner = () => {
+    return [
+      [0, 2, 1],
+      [3, 5, 1],
+      [6, 8, 1],
+    ].some(checkLine);
+  };
+  return { curr, checkWinner };
 })();
